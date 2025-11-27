3bb6f9a8bcce4b511fd292ae8bcedd2ac12b7b89
sande-swalah <alvinswalah@gmail.com>
2025-11-27 20:22:04 +0300

    arrays lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,26 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"]
+function logFirstProduct() {
+  console.log (products[0])
+}
 
+//adding products
+function addProduct(){
+  products.push("Tablet")
+  console.log (products)
+}
+
+//update product name
+function updateProductName (){
+  products[0]="TelePhone"
+  console.log(students [0])
+}
+
+//removeproducts
+function removeLastProduct(){
+  products.splice(0, 0);
+  console.log(students)
+}
 
 
 // Export the necessary parts for testing
